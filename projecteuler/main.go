4816754a103acb38019814667698c548c72a4039
Alexander Kaupanin <kaupanin@gmail.com>
2015-12-07 00:11:38 -0600

    bruteforce solution for problem 8

--- projecteuler/main.go
+++ projecteuler/main.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Println(problem_005([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}))
 	fmt.Println(problem_006(100))
 	fmt.Println(problem_007(10001))
+	fmt.Println(problem_008(13))
 }
