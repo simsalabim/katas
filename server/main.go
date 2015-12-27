f937e2dc4a7ff2715ac3ab3806e0fbe8348a6b11
Alexander Kaupanin <kaupanin@gmail.com>
2015-12-26 22:26:05 -0600

    Specify port in args

--- server/main.go
+++ server/main.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
+	port := "8000"
+	if len(os.Args) > 1 {
+		port_i, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Specified port values is incorrect", err)
+			os.Exit(1)
+		}
+		if port_i <= 0 {
+			fmt.Fprintf(os.Stderr, "Specified port values is incorrect: %s", os.Args[1])
+			os.Exit(1)
+		}
+		port = os.Args[1]
+	}
+	fmt.Println("Starting server on http://0.0.0.0:" + port)
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
