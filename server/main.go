434cca70a2545547bded10920d38e7fcb2a1f588
Alexander Kaupanin <kaupanin@gmail.com>
2015-12-26 22:33:16 -0600

    Server with counter

--- server/main.go
+++ server/main.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	port := "8000"
@@ -23,10 +27,22 @@ func main() {
 		port = os.Args[1]
 	}
 	fmt.Println("Starting server on http://0.0.0.0:" + port)
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
+	switch request.URL.Path {
+	case "/count":
+		fmt.Fprintf(writer, "Count: %d\n", count)
+	case "/favicon.ico":
+	default:
+		{
+			mu.Lock()
+			count++
+			fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
+			mu.Unlock()
+		}
+	}
 }
