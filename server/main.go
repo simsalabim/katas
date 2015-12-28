422f1f651bebca15d7d196f852b1989495c9070d
Alexander Kaupanin <kaupanin@gmail.com>
2015-12-27 23:41:59 -0600

    Use 'flag' for CLI arguments parsing

--- server/main.go
+++ server/main.go
@@ -1,35 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"sync"
 )
 
 var mu sync.Mutex
 var count int
+var port = flag.String("p", "8000", "port")
 
 func main() {
-	port := "8000"
-	if len(os.Args) > 1 {
-		port_i, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Specified port values is incorrect", err)
-			os.Exit(1)
-		}
-		if port_i <= 0 {
-			fmt.Fprintf(os.Stderr, "Specified port values is incorrect: %s", os.Args[1])
-			os.Exit(1)
-		}
-		port = os.Args[1]
-	}
-	fmt.Println("Starting server on http://0.0.0.0:" + port)
+	flag.Parse()
+	fmt.Println("Starting server on http://0.0.0.0:" + *port)
 
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
+	log.Fatal(http.ListenAndServe("localhost:"+*port, nil))
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
